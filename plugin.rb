3dd8ae61527664868d3194357be8d80f560187af
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-05-05 18:55:14 +0500

    Updated message-bus cors

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# name: message-bus-cors
+# about: Adds custom CORS headers to MessageBus if enabled
+# version: 0.1
+# authors: Ahsan Afzal
+
+after_initialize do
+  MessageBus.extra_response_headers_lookup do |env|
+    setup_message_bus_env(env)
+    headers = env["__mb"][:extra_headers]
+    headers["Access-Control-Allow-Origin"] = 
+    headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    headers
+  end
+end
